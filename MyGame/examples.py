29a4b4b2f597534521902c0350e4f6b14fa5b367
YanaPanova <114726090+YanaPanova@users.noreply.github.com>
2023-02-19 16:30:26 +0200

    Add files via upload

---
+++ MyGame/examples.py
@@ -0,0 +1,35 @@
+var = 123 #integer
+PI = 3.14 #float
+text = "Python" #string
+is_gane = True #bool
+
+#три види колекцій в Python
+lst = list() #list
+tup = tuple() #tuple
+dct = dict() #dict
+
+print(var)
+
+var = var - 100
+
+print(var)
+
+text = text + '3'
+
+print(text)
+
+print(text[0])
+
+#text[0] = 'F'
+
+list_text = list(text)
+print(list_text)
+
+list_text[0] = 'F'
+print(list_text)
+
+my_tuple = (1, 2)
+print(my_tuple[0])
+
+my_tuple[0] = 5
+print(my_tuple[0])
